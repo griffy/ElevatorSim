2fcf62c146a239cf9f423d06bf7b7090e2f6a22e
Joel Griffith <joel@joel-griffith.com>
2011-12-03 02:40:26 -0500

    add future events queue

---
+++ feq.py
@@ -0,0 +1,22 @@
+import heapq
+
+class FutureEventsQueue(object):
+    def __init__(self):
+        self._priority_queue = []
+        
+    def schedule_event(self, event):
+        # in case we have two events scheduled to happen at the same time
+        # and we want/need one to happen before the other, we schedule
+        # first according to the time and second according to the number
+        # of the event
+        heapq.heappush(self._priority_queue, 
+                      ((event.time, event.count), event))
+                      
+    def next_event(self):
+        event_tuple = heapq.heappop(self._priority_queue)
+        event = event_tuple[1]
+        return event
+        
+    def has_events(self):
+        return len(self._priority_queue) > 0
+        
