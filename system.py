60ba9899042aad7c73f27ae4e1e6e66f698de08f
Joel Griffith <joel@joel-griffith.com>
2011-12-03 03:41:03 -0500

    add base System class to inherit from

---
+++ system.py
@@ -0,0 +1,31 @@
+class System(object):
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.feq = FutureEventsQueue()
+        self.clock = Clock()
+        self.stats = Stats()
+        self.initialize()
+        
+    def run(self):
+        self.reset()
+        event = self.generate_initial_event()
+        self.feq.schedule_event(event)
+        while feq.has_events():
+            event = self.feq.next_event()
+            self.clock.update(event.time)
+            self.simulate(event)
+        return self.stats
+        
+    def initialize(self):
+        """ System-specific variables are initialized here """
+        pass
+        
+    def generate_initial_event(self):
+        pass
+        
+    def simulate(self, event):
+        """ Updating of system state and stats, and scheduling of next event
+            should go here """
+        pass
