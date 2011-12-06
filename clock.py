d64311bc01e41b6b6329cba6cd516b38a47012ec
Joel Griffith <joel@joel-griffith.com>
2011-12-06 17:41:39 -0500

    add start_time attribute and has_run method

--- clock.py
+++ clock.py
@@ -1,13 +1,22 @@
 class Clock(object):
-    def __init__(self, time=0):
+    def __init__(self, time=None):
         self._time = time
-        
-    def update(self, time):
-        self._time = time
-        
+   
     def increment(self, dt):
-        self._time += dt
-        return self._time
+        self.update(self.time() + dt)
+        return self.time()
+             
+    def update(self, time):
+        if self._time is None:
+            self.start_time = time
+        self._time = time
         
     def time(self):
+        if self._time is None:
+            return 0
         return self._time
+       
+    def has_run(self, duration):
+        if self.time() - self.start_time > duration:
+            return True
+        return False
