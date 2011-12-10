af3ab785f29339268d16269aa7c66a2653d89b06
Joel Griffith <joel@joel-griffith.com>
2011-12-10 16:31:32 -0500

    add basic floor picking based on time and type

--- elevator.py
+++ elevator.py
@@ -1,14 +1,17 @@
+# We use a probability of 0.01 in place of floors that did not have any
+# passengers get off on
+
 TYPE_F_DISTRS = {
-    "morning": generator({18: 1/38.0,
+    'morning': generator({18: 1/38.0,
                           21: 3/38.0,
                           24: 6/38.0,
                           27: 13/38.0,
-                          30: 0.01,    # no one came, but we need to say they can
+                          30: 0.01,
                           33: 1/38.0,
                           36: 14/38.0
                }),
-    "afternoon": None,
-    "evening": generator({18: 0.01,
+    'afternoon': None,
+    'evening': generator({18: 0.01,
                           21: 0.01,
                           24: 2/7.0,
                           27: 1/7.0,
@@ -19,7 +22,7 @@ TYPE_F_DISTRS = {
 }
 
 TYPE_L_DISTRS = {
-    "morning": generator({5: 13/62.0,
+    'morning': generator({5: 13/62.0,
                           8: 17/62.0,
                           10: 7/62.0,
                           12: 2/62.0,
@@ -28,8 +31,8 @@ TYPE_L_DISTRS = {
                           18: 5/62.0,
                           23: 4/62.0
                }),
-    "afternoon": None,
-    "evening": generator({5: 4/13.0,
+    'afternoon': None,
+    'evening': generator({5: 4/13.0,
                           8: 2/13.0,
                           10: 1/13.0,
                           12: 1/13.0,
@@ -41,7 +44,7 @@ TYPE_L_DISTRS = {
 }
 
 TYPE_I_DISTRS = {
-    "morning": generator({3: 0.01,
+    'morning': generator({3: 0.01,
                           4: 0.01,
                           5: 23/71.0,
                           8: 7/71.0,
@@ -52,8 +55,8 @@ TYPE_I_DISTRS = {
                           18: 1/71.0,
                           23: 6/71.0
                }),
-    "afternoon": None,
-    "evening": generator({3: 4/13.0,
+    'afternoon': None,
+    'evening': generator({3: 4/13.0,
                           4: 0.01,
                           5: 0.01,
                           8: 0.01,
@@ -67,7 +70,7 @@ TYPE_I_DISTRS = {
 }
 
 TYPE_E_DISTRS = {
-    "morning": generator({3: 4/28.0,
+    'morning': generator({3: 4/28.0,
                           4: 3/28.0,
                           5: 1/28.0,
                           6: 2/28.0,
@@ -101,8 +104,8 @@ TYPE_E_DISTRS = {
                           35: 0.01,
                           36: 0.01
                }),
-    "afternoon": None,
-    "evening": generator({3: 4/10.0,
+    'afternoon': None,
+    'evening': generator({3: 4/10.0,
                           4: 3/10.0,
                           5: 0.01,
                           6: 0.01,
@@ -138,5 +141,35 @@ TYPE_E_DISTRS = {
                })
 }
 
+distrs = [
+    TYPE_F_DISTRS,
+    TYPE_L_DISTRS,
+    TYPE_I_DISTRS,
+    TYPE_E_DISTRS
+]
+
+TYPE_F = 0
+TYPE_L = 1
+TYPE_I = 2
+TYPE_E = 3
+
 class Elevator(object):
-    def __init__(self, 
+    def __init__(self, type_):
+        self.type = type_
+        
+    def pick_floor(time):
+        """ picks a floor it is going to based on the time of day and type
+            time must be in seconds where 0 is 5am
+        """
+        
+        morning_end = 6*60*60 # 11am
+        afternoon_end = morning_end + 6*60*60
+        evening_end = afternoon_end + 6*60*60
+        
+        if 0 <= time <= morning_end: #11am
+            return distrs[self.type]['morning']()
+        elif morning_end < time <= afternoon_end:
+            return distrs[self.type]['afternoon']()
+        elif afternoon_end < time <= evening_end:
+            return distrs[self.type]['evening']()
+            
